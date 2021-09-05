b8178ccb3457c117b2a4f0a758d3c4c69556eef8
marquise <mwabbey3@hotmail.com>
2021-09-04 20:43:22 -0400

    added local files

---
+++ Conditionals/app2.js
@@ -0,0 +1,14 @@
+let phrase = 'stop'
+
+if (phrase === 'stop') {
+    console.log('red')
+}
+else -if (phrase === 'slow') {
+    console.log('yellow')
+}
+else -if (phrase === 'go') {
+    console.log(green)
+}
+else {
+    console.log('purple')
+}
